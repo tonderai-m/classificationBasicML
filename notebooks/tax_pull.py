5bbff0a72d51b6c6e60672b29d85c302764cf0cf
tonderai-m <tonderaimadamba@gmail.com>
2023-07-30 15:49:20 -0500

    basic ml

--- notebooks/tax_pull.py
+++ notebooks/tax_pull.py
@@ -1,7 +1,7 @@
 class import_data: 
     def get_df(): 
         import pandas as pd 
-        DF1 = pd.read_excel('/Users/tonderaimadamba/Documents/Client Work/Ryan/tax mapper/client_data/Dataset1_SamplePopulation_CandyBeverages_Compiled.xlsx',skiprows = range(0, 3),)
-        DF2 = pd.read_excel('/Users/tonderaimadamba/Documents/Client Work/Ryan/tax mapper/client_data/SamplePopulation_Albertsons_CandyBeverages_Compiled.xlsx',skiprows = range(0, 3),usecols=('B:H'),dtype={'UPC': str})
-        DF3 = pd.read_excel('/Users/tonderaimadamba/Documents/Client Work/Ryan/tax mapper/client_data/SamplePopulation_Albertsons_HealthBeauty_Compiled.xlsx',skiprows = range(0, 3),usecols=('B:H'),dtype={'UPC': str})
+        DF1 = pd.read_excel('Compiled_1.xlsx',skiprows = range(0, 3),)
+        DF2 = pd.read_excel('Compiled_2.xlsx',skiprows = range(0, 3),usecols=('B:H'),dtype={'UPC': str})
+        DF3 = pd.read_excel('Compiled_3.xlsx',skiprows = range(0, 3),usecols=('B:H'),dtype={'UPC': str})
         return DF1, DF2, DF3
